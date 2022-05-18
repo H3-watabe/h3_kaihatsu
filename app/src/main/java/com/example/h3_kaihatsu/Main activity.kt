165d0af8a0ad067e97881625e9ab6a724ac8b3ee
arisa <tianzhogg00@gmail.com>
2022-05-18 13:30:01 +0900

    Search

---
+++ app/src/main/java/com/example/h3_kaihatsu/Main activity.kt
@@ -0,0 +1,11 @@
+package com.example.h3_kaihatsu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class `Main activity` : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
